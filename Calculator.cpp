3e08e57c08476480e81ac3c0592d22e11b19007c
Jawad Mughal <jawadmughaldev@gmail.com>
2024-01-25 22:26:29 +0500

    implement the subracton funciton

--- Calculator.cpp
+++ Calculator.cpp
@@ -6,6 +6,9 @@ public:
    double addition(double num1, double num2) {
         return num1 + num2;
     }
+      double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
 };
 
 int main() {
